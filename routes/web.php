bca39ae95073a2f75a5ee1727dbb07afe863cbce
qunoot zehra <qunootzehra21@gmail.com>
2026-07-22 21:12:56 +0500

    commit by qunoot

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
 Route::resource('students', StudentController::class);
+Route::resource('/', StudentController::class);
+
 
